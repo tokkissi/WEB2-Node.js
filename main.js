cd49499f2fd55f3548365d510d6686cc32486e2a
tokkissi <alal4674@gmail.com>
2022-08-18 14:59:00 +0900

    Node.js-글목록 출력하기

--- main.js
+++ main.js
@@ -6,70 +6,71 @@ var app = http.createServer(function (request, response) {
   // url.parse 를 대체할 새로운 기술로 절대경로를 인자로 갖는 URL 객체를 이용한다
   const queryData = new URL("http://localhost:3000" + _url);
   // var queryData = url.parse(_url, true).query;  // 더이상 사용, 지원되지 않는다
-  console.log(queryData);
   // 쿼리로 받아온 pathname 이 '/' 면 다음을 실행
   if (queryData.pathname === "/") {
     // 쿼리로 받아온 id 값이 null 이면 다음을 실행
     if (queryData.searchParams.get("id") === null) {
-      fs.readFile(
-        `data/${queryData.searchParams.get("id")}`,
-        "utf-8",
-        (err, description) => {
-          var title = "Welcome";
-          var description = "Hello, Node.js";
-          var template = `
-  <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="?id=HTML">HTML</a></li>
-    <li><a href="?id=CSS">CSS</a></li>
-    <li><a href="?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>${description}</p>
-</body>
-</html>
-  `;
-          response.writeHead(200); //서버의 상태코드를 반환한다. 정상적으로 페이지를 찾을 수 있으므로 정상 상태를 나타내는 200 을 리턴한다
-          response.end(template); // 클라이언트에게 괄호 안의 데이터를 보내고 응답을 끝낸다
+      fs.readdir("./data", (error, filelist) => {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = "<ul>";
+        for (let i = 0; i < filelist.length; i++) {
+          list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
         }
-      );
-      // 쿼리로 받아온 id 값이 null 이 아니면 다음을 실행
+        list += "</ul>";
+        var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200); //서버의 상태코드를 반환한다. 정상적으로 페이지를 찾을 수 있으므로 정상 상태를 나타내는 200 을 리턴한다
+        response.end(template); // 클라이언트에게 괄호 안의 데이터를 보내고 응답을 끝낸다
+        // 쿼리로 받아온 id 값이 null 이 아니면 다음을 실행
+      });
     } else {
-      fs.readFile(
-        `data/${queryData.searchParams.get("id")}`,
-        "utf-8",
-        (err, description) => {
-          var title = queryData.searchParams.get("id");
-          var template = `
-  <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="?id=HTML">HTML</a></li>
-    <li><a href="?id=CSS">CSS</a></li>
-    <li><a href="?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>${description}</p>
-</body>
-</html>
-  `;
-          response.writeHead(200); //서버의 상태코드를 반환한다. 정상적으로 페이지를 찾을 수 있으므로 정상 상태를 나타내는 200 을 리턴한다
-          response.end(template); // 클라이언트에게 괄호 안의 데이터를 보내고 응답을 끝낸다
+      fs.readdir("./data", (error, filelist) => {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = "<ul>";
+        for (let i = 0; i < filelist.length; i++) {
+          list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
         }
-      );
+        list += "</ul>";
+        fs.readFile(
+          `data/${queryData.searchParams.get("id")}`,
+          "utf-8",
+          (err, description) => {
+            var title = queryData.searchParams.get("id");
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+              `;
+            response.writeHead(200); //서버의 상태코드를 반환한다. 정상적으로 페이지를 찾을 수 있으므로 정상 상태를 나타내는 200 을 리턴한다
+            response.end(template); // 클라이언트에게 괄호 안의 데이터를 보내고 응답을 끝낸다
+          }
+        );
+      });
     }
   } else {
     response.writeHead(404); //  // 서버의 상태코드를 반환한다. 페이지를 찾을 수 없다면 해당 상태인 상태 코드 404 을 리턴한다
