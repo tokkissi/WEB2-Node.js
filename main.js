a13d1d0471e251d2f6ab1cfec5b73ee3e8f82256
tokkissi <alal4674@gmail.com>
2022-08-23 13:11:27 +0900

    Node.js-31.App 제작-글생성 UI 만들기

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var http = require("http");
 var fs = require("fs");
 function templateHTML(title, list, body) {
-  return `<!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            ${body}
-          </body>
-          </html>`;
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+  `;
 }
 
 function templateList(filelist) {
@@ -30,6 +33,7 @@ var app = http.createServer(function (request, response) {
   // url.parse 를 대체할 새로운 기술로 절대경로를 인자로 갖는 URL 객체를 이용한다
   const queryData = new URL("http://localhost:3000" + _url);
   // var queryData = url.parse(_url, true).query;  // 더이상 사용, 지원되지 않는다
+  console.log(queryData.pathname);
   // 쿼리로 받아온 pathname 이 '/' 면 다음을 실행
   if (queryData.pathname === "/") {
     // 쿼리로 받아온 id 값이 null 이면 다음을 실행
@@ -68,6 +72,29 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
+  } else if (queryData.pathname === "/create") {
+    fs.readdir("./data", (error, filelist) => {
+      var title = "Welcome";
+      var description = "Hello, Node.js";
+      var list = templateList(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:3000/process_create" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404); //  // 서버의 상태코드를 반환한다. 페이지를 찾을 수 없다면 해당 상태인 상태 코드 404 을 리턴한다
     response.end("not found"); // 클라이언트에게 괄호 안의 데이터를 보내고 응답을 끝낸다. 페이지를 찾지 못했다는 의미의 not found 를 적어주었다
