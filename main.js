bd470b8d6c30d395f557caf8a5b61131504e9ec5
tokkissi <alal4674@gmail.com>
2022-09-01 19:22:45 +0900

    Node.js-45.App 제작 - 모듈의 활용

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
 var http = require("http");
 var fs = require("fs");
-
-let template = {
-  list: function (filelist) {
-    var list = "<ul>";
-    for (let i = 0; i < filelist.length; i++) {
-      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    }
-    list += "</ul>";
-    return list;
-  },
-  HTML: function (title, list, body, control) {
-    return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>
-  `;
-  },
-};
+let template = require("./lib/template.js");
 
 var app = http.createServer(function (request, response) {
   // var url = require("url");  // url.parse 가 지원안되므로 불필요
